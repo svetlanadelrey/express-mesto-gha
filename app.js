3ea7cfd6bec6259901e9c7511293167257084fbb
Svetlana <kuznetsova@66.ru>
2023-06-14 23:59:19 +0500

    feat: создать контроллер и роут для получения информации о пользователе

--- app.js
+++ app.js
@@ -13,21 +13,11 @@ mongoose.connect('mongodb://127.0.0.1/mestodb');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6479de7080c3f4edb2384332',
-  };
-
-  next();
-});
-
 app.post('/signin', loginUser);
 app.post('/signup', createUser);
 
-app.use(auth);
-
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
+app.use('/users', auth, userRouter);
+app.use('/cards', auth, cardRouter);
 
 app.all('/*', (req, res) => {
   res.status(NOT_FOUND).send({ message: 'Запрашиваемая страница не существует' });
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,24 @@
 const jwt = require('jsonwebtoken');
 
+const extractBearerToken = (header) => {
+  header.replace('Bearer ', '');
+};
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new Error('Необходима авторизация');
+    res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
-  const token = authorization.replace('Bearer ', '');
+  const token = extractBearerToken(authorization);
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new Error('Необходима авторизация');
+    res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
   next();

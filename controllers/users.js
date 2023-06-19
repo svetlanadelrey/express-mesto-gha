5146c903989c472989a6a476210c314582d6a0d9
Svetlana <kuznetsova@66.ru>
2023-06-19 11:59:57 +0500

    hotfix: ошибки автотестов

--- controllers/users.js
+++ controllers/users.js
@@ -63,9 +63,6 @@ const loginUser = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       res.send({ token });
     })
-    .catch(() => {
-      throw new BadRequestError('Неправильные почта или пароль');
-    })
     .catch(next);
 };
 
